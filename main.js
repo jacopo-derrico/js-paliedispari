68bdc40974bff854a65dbf13f96361c3d8fe9534
jacopo-derrico <derricoj.design@gmail.com>
2024-01-15 17:18:57 +0100

    odds and evens game completed

--- main.js
+++ main.js
@@ -39,3 +39,42 @@ bottonePalindromo.addEventListener('click', function() {
         risultatoPalindromo.innerHTML = `La parola ${parolaUtente.value} non è palindroma`
     } 
  })
+
+/*L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+Sommiamo i due numeri
+Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+Dichiariamo chi ha vinto.
+*/
+
+let numeroUtente = document.getElementById('numero')
+let even = document.getElementById('pari')
+let odd = document.getElementById('dispari')
+
+let bottoneNumeri = document.getElementById('pari-dispari')
+let risultatoNumeri = document.getElementById('result-number')
+
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * ( max - min + 1 ) + min )
+}
+
+function isEven(number) {
+    if ((number % 2) === 0) {
+        return true
+    }
+    return false
+}
+
+bottoneNumeri.addEventListener('click', function() {
+    let somma = parseInt(numeroUtente.value) + randomNumber(1, 5)
+
+    isEven(somma)
+    console.log(isEven)
+    if (isEven === true && even.checked ) {
+        risultatoNumeri.innerHTML = `Il tuo numero è: ${numeroUtente.value}. Il numero del computer è: ${somma - parseInt(numeroUtente.value)}. Hai vinto! La somma dei numeri è: ${somma}`
+    } else if (isEven === false && odd.checked ) {
+        risultatoNumeri.innerHTML = `Il tuo numero è: ${numeroUtente.value}. Il numero del computer è: ${somma - parseInt(numeroUtente.value)}. Hai vinto! La somma dei numeri è: ${somma}`
+    } else {
+        risultatoNumeri.innerHTML = `Il tuo numero è: ${numeroUtente.value}. Il numero del computer è: ${somma - parseInt(numeroUtente.value)}. Hai perso :( La somma dei numeri è: ${somma}`
+    }
+})
